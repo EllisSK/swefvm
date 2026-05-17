47826edc04d5205794c2f784938948e0ce4f077b
EllisSK <ellissinclairkent@gmail.com>
2026-05-17 09:45:55 +0100

    refactor: refactor temporal integrator classes
    Refactored temporal integrator classes to be agnostic to number of dimensions

--- src/swefvm/methods/temporal/base.py
+++ src/swefvm/methods/temporal/base.py
@@ -2,5 +2,5 @@ from abc import ABC, abstractmethod
 
 class TemporalIntegrator(ABC):
     @abstractmethod
-    def integrate(self, mesh, physics, spatial, riemann, bcs, dt):
+    def integrate(self, mesh, physics, spatial, riemann, external_bcs, internal_bcs, dt):
         pass
