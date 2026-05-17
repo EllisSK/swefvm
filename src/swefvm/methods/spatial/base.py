d7e4fe78ca8cd7b937aa51f0419720d793e05d19
EllisSK <ellissinclairkent@gmail.com>
2026-05-17 09:42:07 +0100

    refactor: refactor spatial reconstructor classes
    Refactored spatial reconstructor classes to be agnostic to number of dimensions

--- src/swefvm/methods/spatial/base.py
+++ src/swefvm/methods/spatial/base.py
@@ -2,5 +2,10 @@ from abc import ABC, abstractmethod
 
 class SpatialReconstruction(ABC):
     @abstractmethod
-    def reconstruct_conserved_variables(self, mesh) -> tuple:
+    def reconstruct_conserved_variables(self, mesh, direction: int = 0) -> tuple:
         pass
+
+def axis_slice(ndim: int, axis: int, start, stop) -> tuple:
+    sl = [slice(None)] * ndim
+    sl[axis] = slice(start, stop)
+    return tuple(sl)
