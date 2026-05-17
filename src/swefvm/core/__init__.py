8679fecc1aa0774a2119b4cc1a0197487e450d22
EllisSK <ellissinclairkent@gmail.com>
2026-05-17 23:58:28 +0100

    feat: add 2D mesh class
    Added 2D mesh class to enable solving the SWEs in 2D.

--- src/swefvm/core/__init__.py
+++ src/swefvm/core/__init__.py
@@ -1,3 +1,3 @@
 from .simulation import Simulation
-from .mesh import Mesh1D
+from .mesh import Mesh1D, Mesh2D
 from .boundaries import FixedFlowBoundary, FixedDepthBoundary, VariableDepthBoundary, VariableFlowBoundary, VariableConservedBoundary
