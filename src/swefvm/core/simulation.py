dea8ebf2bfe653232ec89af1d7262b048179e3ef
EllisSK <ellissinclairkent@gmail.com>
2026-05-17 03:30:35 +0100

    fix: fix relative import bug
    Fixed relative import bug in a number of files that remained from original fork.

--- src/swefvm/core/simulation.py
+++ src/swefvm/core/simulation.py
@@ -5,10 +5,10 @@ from collections.abc import Sequence
 from .mesh import Mesh
 from .boundaries import BoundaryCondition, ExternalBoundary, InternalBoundary
 
-from src.swefvm.methods.spatial import SpatialReconstruction
-from src.swefvm.methods.temporal import TemporalIntegrator
-from src.swefvm.methods.riemann_solvers import RiemannSolver
-from src.swefvm.physics import Physics
+from swefvm.methods.spatial import SpatialReconstruction
+from swefvm.methods.temporal import TemporalIntegrator
+from swefvm.methods.riemann_solvers import RiemannSolver
+from swefvm.physics import Physics
 
 class Simulation:
     def __init__(self, mesh: Mesh, physics: Physics, spatial: SpatialReconstruction, temporal: TemporalIntegrator, riemann: RiemannSolver, bcs: list[BoundaryCondition]):
--- src/swefvm/utils/io.py
+++ src/swefvm/utils/io.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 from pathlib import Path
 
-from src.swefvm.core.simulation import Simulation
-from src.swefvm.core.mesh import Mesh1D
+from swefvm.core.simulation import Simulation
+from swefvm.core.mesh import Mesh1D
 
 def write_1D_simulation_results(simulation: Simulation, output_path: Path, name: str):
     if not isinstance(simulation.mesh, Mesh1D):
